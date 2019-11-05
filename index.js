4d889dbceef07a8ab975921e06bd718ecc51ba4d
Brandon Nichols <brandonaronnichols@gamil.com>
2019-11-04 23:35:00 -0600

    basic setup of server

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code away!
+const express = require("express");
+
+const userRouter = require("./users/userRouter");
+const postRouter = require("./posts/postRouter");
+
+server = express();
+
+server.use(express.json());
+server.use("/api/users", userRouter);
+server.use("/api/posts", postRouter);
+
+server.listen(4000, () => {
+  console.log("server listening on http://localhost:4000/");
+});
